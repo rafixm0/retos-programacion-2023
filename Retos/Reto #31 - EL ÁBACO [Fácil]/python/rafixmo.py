38e181d9882b452b15a7a64b65487584737019b6
rafixm0 <123590944+rafixm0@users.noreply.github.com>
2023-08-01 23:20:59 +0200

    Create rafixmo.py

---
+++ Retos/Reto #31 - EL ÁBACO [Fácil]/python/rafixmo.py
@@ -0,0 +1,37 @@
+# Reto de programación de MoureDev #31
+
+entrada = ["O---OOOOOOOO",
+    "OOO---OOOOOO",
+    "---OOOOOOOOO",
+    "OO---OOOOOOO",
+    "OOOOOOO---OO",
+    "OOOOOOOOO---",
+    "---OOOOOOOOO"]
+
+# Resultado: 1.302.790
+
+def obtenerNumeroAbacco(numero):
+    total = 0;
+    for car in numero:
+        if car == '-':
+            return total
+        else:
+            total += 1
+    return total
+    
+
+def contarAbaco (abacco):
+    i = 1
+    resultado = []
+
+    for numero in abacco:
+        resultado.append(obtenerNumeroAbacco(numero))
+        if (i == 1 or i == 5): 
+            resultado.append('.')
+            i += 1
+        i+=1
+
+    print(''.join(str(e) for e in resultado))
+
+
+contarAbaco(entrada)
